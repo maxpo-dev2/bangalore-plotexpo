3c6a2dc54cd508e49ebcd0a28590d9428d56d4b3
machumzd <www.machuzz123@gmail.com>
2026-03-13 17:33:16 +0530

    meeting apis added

--- app/components/home/HeroSection.tsx
+++ app/components/home/HeroSection.tsx
@@ -1,23 +1,25 @@
 import { zenTokyoZoo } from "@/app/fonts";
 import DateBadge from "./DateBadge";
+import Countdown from "./CountDown";
 
 const HeroSection = () => {
   return (
     <section
-      className="relative h-screen bg-center bg-cover bg-[#C6FFC2] bg-no-repeat "
+      className="relative h-screen bg-center bg-cover overflow-x-hidden bg-[#C6FFC2] bg-no-repeat"
       style={{
         backgroundImage: "url('/images/hero-banner.png')",
       }}
     >
-      <div className="absolute inset-0"></div>
-      <div className="relative z-10  h-full flex flex-col justify-start pt-[14vh]">
+      <div className="relative z-10 h-full flex flex-col justify-start pt-[14vh]">
         <div className="flex justify-center items-center">
           <h1
-            className={`${zenTokyoZoo.className} text-3xl  md:text-8xl font-bold leading-[1] text-primary inline-block whitespace-normal `}
+            className={`${zenTokyoZoo.className} text-3xl md:text-8xl font-bold leading-[1] text-primary`}
           >
             Bengaluru Plot Expo 2026
           </h1>
         </div>
+
+        <Countdown targetDate="2026-03-14T09:30:00+05:30" />
         <DateBadge />
       </div>
     </section>
